65b6299a680280e4490b38a18bd6b00cae0f21e0
mathieu <mathieu@cogimator.net>
2014-11-08 21:25:20 +0100

    Nested generator

---
+++ SampleDataGenerator.Tests/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleDataGenerator.Tests
+{
+    using System;
+
+    public class Address
+    {
+        public string Country { get; set; }
+    }
+}
--- SampleDataGenerator.Tests/GeneratorTest.cs
+++ SampleDataGenerator.Tests/GeneratorTest.cs
@@ -9,7 +9,7 @@
     public class GeneratorTest
     {
         [TestMethod]
-        public void SampleData()
+        public void SimpleObject()
         {
             // arrange
             var count = 50;
@@ -37,6 +37,53 @@
             // assert
             foreach (var d in data)
             {
+                Assert.IsTrue(firstNames.Contains(d.FirstName));
+                Assert.IsTrue(lastNames.Contains(d.LastName));
+                Assert.IsFalse(d.Id == default(Guid));
+            }
+
+            Assert.AreEqual(count, data.Count);
+        }
+
+        [TestMethod]
+        public void NestedObjects()
+        {
+            // arrange
+            var count = 50;
+
+            var countries = StaticData.Countries;
+            var firstNames = StaticData.FirstNames;
+            var lastNames = StaticData.LastNames;
+
+            var adressGenerator =
+                Generator
+                    .For<Address>()
+                    .For(x => x.Country)
+                        .ChooseRandomlyFrom(StaticData.Countries);
+
+            var clientGenerator =
+                Generator
+                    .For<Client>()
+                    .For(x => x.FirstName)
+                        .ChooseFrom(StaticData.FirstNames)
+                    .For(x => x.DateOfBirth)
+                        .Range(DateTime.Now, DateTime.Now.AddYears(1))
+                    .For(x => x.LastName)
+                        .ChooseFrom(StaticData.LastNames)
+                    .For(x => x.Company)
+                        .ChooseFrom(StaticData.Companies)
+                    .For(x => x.Address)
+                        .CreateUsing(adressGenerator)
+                    .For(x => x.Id)
+                        .CreateUsing(() => Guid.NewGuid());
+
+            // act
+            var data = clientGenerator.Generate(50).ToList();
+
+            // assert
+            foreach (var d in data)
+            {
+                Assert.IsTrue(countries.Contains(d.Address.Country));
                 Assert.IsTrue(firstNames.Contains(d.FirstName));
                 Assert.IsTrue(lastNames.Contains(d.LastName));
                 Assert.IsFalse(d.Id == default(Guid));
