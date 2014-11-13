ca38b7476100d5e4debdb46b60114f78dbaf463c
mathieu <mathieu@cogimator.net>
2014-11-14 00:13:39 +0100

    Moved enumerable extensions to generators

---
+++ SampleDataGenerator.Tests/Tests/FuncGeneratorTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleDataGenerator.Generators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleDataGenerator.Tests.Tests
+{
+    [TestClass]
+    public class FuncGeneratorTest
+    {
+        [TestMethod]
+        public void Function_passed_as_argument_is_called()
+        {
+            // arrange
+            var expected = new object();
+            var generator = new FuncGenerator<object>(() => expected);
+
+            // act
+            var generated = generator.Get();
+
+            // assert
+            Assert.AreSame(expected, generated);
+        }
+    }
+}
---
+++ SampleDataGenerator.Tests/Tests/ObjectGeneratorGeneratorTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SampleDataGenerator.Generators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleDataGenerator.Tests.Tests
+{
+    [TestClass]
+    public class ObjectGeneratorGeneratorTest
+    {
+        [TestMethod]
+        public void Generator_passed_as_argument_is_called()
+        {
+            // arrange
+            var expected = new object();
+            var mock = Mock.Of<IObjectGenerator<object>>();
+            Mock.Get(mock).Setup(x => x.Generate()).Returns(expected);
+
+            var generator = new ObjectGeneratorGenerator<object>(mock);
+
+            // act
+            var generated = generator.Get();
+
+            // assert
+            Assert.AreSame(expected, generated);
+            Mock.Get(mock).Verify(x => x.Generate(), Times.Once());
+        }
+    }
+}
