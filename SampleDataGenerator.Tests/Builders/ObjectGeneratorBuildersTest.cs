e9728cf37271b1771d06fb17b5834a44ab1a5dbf
Mathieu BRUN <mathieubrun@users.noreply.github.com>
2014-11-22 19:17:41 +0100

    Test coverage

---
+++ SampleDataGenerator.Tests/Builders/ObjectGeneratorBuildersTest.cs
@@ -0,0 +1,28 @@
+﻿namespace SampleDataGenerator.Tests.Builders
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using SampleDataGenerator.Builders;
+
+    [TestClass]
+    public class ObjectGeneratorBuildersTest
+    {
+        [TestMethod]
+        public void ObjectGeneratorBuilder_Must_return_correct_builder()
+        {
+            // arrange
+            var sut = new ObjectGeneratorBuilder<Person>();
+
+            // act
+            var dateBuilder = sut.For(x => x.DateOfBirth);
+            var propertyBuilder = sut.For(x => x.FirstName);
+            var arrayBuilder = sut.For(x => x.Relatives);
+
+            // assert
+            Assert.IsInstanceOfType(dateBuilder, typeof(DatePropertyGeneratorBuilder<Person>));
+            Assert.IsInstanceOfType(propertyBuilder, typeof(PropertyGeneratorBuilder<Person, string>));
+            Assert.IsInstanceOfType(arrayBuilder, typeof(ArrayPropertyGeneratorBuilder<Person, Person>));
+        }
+    }
+}
