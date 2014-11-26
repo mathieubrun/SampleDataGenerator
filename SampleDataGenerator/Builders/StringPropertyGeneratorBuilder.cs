42039bdb7655170961adef4278ac3328e8071e7b
Mathieu BRUN <mathieubrun@users.noreply.github.com>
2014-11-26 21:14:49 +0100

    Refactored stringgenerator

--- SampleDataGenerator/Builders/StringPropertyGeneratorBuilder.cs
+++ SampleDataGenerator/Builders/StringPropertyGeneratorBuilder.cs
@@ -7,8 +7,6 @@ namespace SampleDataGenerator.Builders
 {
     public class StringPropertyGeneratorBuilder<TObj> : PropertyGeneratorBuilder<TObj, string>, IStringPropertyGeneratorBuilder<TObj>
     {
-        private readonly Random rnd = new Random();
-
         public StringPropertyGeneratorBuilder(ObjectGeneratorBuilder<TObj> from, Expression<Func<TObj, string>> expr)
             : base(from, expr)
         {
@@ -16,7 +14,7 @@ namespace SampleDataGenerator.Builders
 
         public IObjectGeneratorBuilder<TObj> LoremIpsum(int sentences)
         {
-            var pgen = new StringGenerator(new RandomSource<string>(StaticData.LoremIpsum), sentences);
+            var pgen = new StringGenerator(new RandomSource<string>(StaticData.LoremIpsum), sentences, " ");
 
             return this.Add(pgen);
         }
