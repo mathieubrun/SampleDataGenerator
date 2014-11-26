5c24aa3e5453b42602e15c788dd637c106b11dc5
mathieu <mathieu@cogimator.net>
2014-11-26 20:27:24 +0100

    Lorem ipsum generator

---
+++ SampleDataGenerator/Generators/StringGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace SampleDataGenerator.Generators
+{
+    public class StringGenerator : IPropertyGenerator<string>
+    {
+        private static readonly Random Rnd = new Random();
+
+        private readonly IEnumerable<string> source;
+        private readonly int count;
+
+        public StringGenerator(string[] source, int count)
+        {
+            this.source = source;
+            this.count = count;
+        }
+
+        public string Get()
+        {
+            return string.Join(" ", this.source.OrderBy(x => Rnd.Next()).Take(count).ToArray());
+        }
+    }
+}
