de7a5d2aac28f9b3315801c57f0e1e6b56a95134
mathieu <mathieu@cogimator.net>
2014-11-26 21:05:51 +0100

    Added ISource concept

--- SampleDataGenerator/Generators/StringGenerator.cs
+++ SampleDataGenerator/Generators/StringGenerator.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using SampleDataGenerator.Sources;
 
 namespace SampleDataGenerator.Generators
 {
     public class StringGenerator : IPropertyGenerator<string>
     {
-        private static readonly Random Rnd = new Random();
-
-        private readonly IEnumerable<string> source;
+        private readonly ISource<string> source;
         private readonly int count;
 
-        public StringGenerator(string[] source, int count)
+        public StringGenerator(ISource<string> source, int count)
         {
             this.source = source;
             this.count = count;
@@ -20,7 +19,7 @@ namespace SampleDataGenerator.Generators
 
         public string Get()
         {
-            return string.Join(" ", this.source.OrderBy(x => Rnd.Next()).Take(count).ToArray());
+            return string.Join(" ", this.source.Get(count).ToArray());
         }
     }
 }
---
+++ SampleDataGenerator/Sources/RandomSource.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SampleDataGenerator.Sources
+{
+    public class RandomSource<TProp> : ISource<TProp>
+    {
+        private static readonly Random Rnd = new Random();
+
+        private readonly TProp[] source;
+
+        public RandomSource(params TProp[] source)
+        {
+            this.source = source;
+        }
+
+        public TProp Get()
+        {
+            return this.source[Rnd.Next(this.source.Length)];
+        }
+
+        public IEnumerable<TProp> Get(int count)
+        {
+            // TODO : optimize
+            return this.Loop().Take(count);
+        }
+
+        private IEnumerable<TProp> Loop()
+        {
+            var list = this.source.OrderBy(x => Rnd.Next());
+
+            var enumerator = list.GetEnumerator();
+
+            if (!list.Any())
+            {
+                yield break;
+            }
+
+            while (enumerator.MoveNext())
+            {
+                yield return enumerator.Current;
+            }
+
+            var q = this.Loop().GetEnumerator();
+
+            while (q.MoveNext())
+            {
+                yield return q.Current;
+            }
+        }
+    }
+}
