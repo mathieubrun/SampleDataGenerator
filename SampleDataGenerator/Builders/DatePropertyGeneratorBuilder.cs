c1abb39e3865f4a0b1056b46d17996c63200d8fa
Mathieu BRUN <mathieubrun@users.noreply.github.com>
2015-01-07 22:18:46 +0100

    Fix build

--- SampleDataGenerator/Builders/DatePropertyGeneratorBuilder.cs
+++ SampleDataGenerator/Builders/DatePropertyGeneratorBuilder.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using SampleDataGenerator.Generators;
 using SampleDataGenerator.Sources;
 
@@ -13,10 +15,30 @@ namespace SampleDataGenerator.Builders
         {
         }
 
+        public DatePropertyGeneratorBuilder(ObjectGeneratorBuilder<TObj> from, Expression<Func<TObj, DateTime?>> expr)
+            : base(from)
+        {
+            var memberExpression = (MemberExpression)expr.Body;
+            var property = (PropertyInfo)memberExpression.Member;
+            var setMethod = property.GetSetMethod();
+
+            var objectParameter = Expression.Parameter(typeof(TObj), "x");
+            var propertyParameter = Expression.Parameter(typeof(DateTime), "y");
+
+            var cast = Expression.TypeAs(propertyParameter, typeof(DateTime?));
+
+            var newEpxression =
+                Expression.Lambda<Action<TObj, DateTime>>(
+                    Expression.Call(objectParameter, setMethod, cast),
+                    objectParameter,
+                    propertyParameter);
+
+            this.Expr = newEpxression;
+        }
+
         public IObjectGeneratorBuilder<TObj> Range(DateTime start, DateTime end)
         {
             var gen = new DateGenerator(start, end);
-
             var pgen = new FuncGenerator<DateTime>(() => gen.Generate());
 
             return this.Add(pgen);
