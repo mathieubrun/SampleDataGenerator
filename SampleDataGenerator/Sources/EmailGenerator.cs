4754c5a494d537dc01d7a919175d0b8267268256
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2015-08-16 11:06:35 +0300

    Add domains and email generator

---
+++ SampleDataGenerator/Sources/EmailGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace SampleDataGenerator.Sources
+{
+    public class EmailGenerator : IElementGenerator<string>
+    {
+        public string Generate()
+        {
+            var firstName = new ArrayRandomizer<string>(StaticData.FirstNames);
+            var domain = new ArrayRandomizer<string>(StaticData.Domains);
+
+            return string.Format("{0}@{1}", firstName.Generate(1).FirstOrDefault(), domain.Generate(1).FirstOrDefault());
+        }
+    }
+}
